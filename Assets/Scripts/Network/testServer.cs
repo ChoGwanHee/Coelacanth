e7d2efbf95fda13cfed1a2b17bd0357248ac6b82
ChoGwanHee <enycal@gmail.com>
2018-06-07 06:38:44 +0900

    .NET 3.5 dll test play -> BUG

---
+++ Assets/Scripts/Network/testServer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ServerModule;
+
+public class testServer : MonoBehaviour
+{
+    ServerModule.ConnectServer _server;
+    private void Start()
+    {
+        Debug.Log("Used .dll");
+        _server = new ServerModule.ConnectServer();
+        Debug.Log("END .dll");
+    }
+}
