5efe9e3ecb006ab879280d5de81bac62982fc2b9
ChoGwanHee <enycal@gmail.com>
2018-06-13 21:03:18 +0900

    ServerModule class library function for connected comfirm

--- Assets/Scripts/Network/testServer.cs
+++ Assets/Scripts/Network/testServer.cs
@@ -3,57 +3,78 @@ using System.Collections.Generic;
 using UnityEngine;
 using ServerModule;
 using System;
+using System.Threading;
 
 public class testServer : MonoBehaviour
 {
+    private void Awake()
+    {
+        Debug.Log("초기화");
+        ServerManager.Initialized();
+    }
 
-    //ServerModule.ServerManager _server = new ServerModule.ServerManager();
-    
-    //private void Start()
-    //{
-    //    try
-    //    {            
-    //        _server.Connect("127.0.0.1", 2020);
-    //    }
-    //    catch (Exception ex)
-    //    {
-    //        Debug.Log(ex.ToString());
-    //        throw;
-    //    }
-    //    Debug.Log("Connected To Server");
+    private void Start()
+    {
+        StartConnectServer("127.0.0.1", 2020);
+    }
+
+    private void StartConnectServer(string _address, int _port)
+    {
+        Debug.Log("서버 접속 시도");
+        ServerManager.Disconnect();
         
-    //    //StartCoroutine(PacketProc());
-    //}
+        try
+        {
+            ServerManager.TCPConnect(_address, _port);
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex.ToString());
+            throw;
+        }
+        StartCoroutine(PacketProc());
+    }
 
-    //IEnumerator PacketProc()
-    //{
-    //    while (true)
-    //    {
-    //        if (Instance.TCP.Available > 0)
-    //        {
-    //            byte[] _buffer = new byte[4096];
-    //            int _read = Instance.TCP.Receive(_buffer, Instance.TCP.Available, 0);
-    //            if (_read > 0)
-    //            {
-    //                Buffer.BlockCopy(_buffer, 0, Instance.BufferSize, Instance.Receive, _read);
-    //                Instance.Receive += _read;
-    //                while (true)
-    //                {
-    //                    int _length = BitConverter.ToInt16(Instance.BufferSize, 0);
-    //                    if (_length > 0 && Instance.Receive >= _length)
-    //                    {
-    //                        _server.ParsePacket(_length);
-    //                        Instance.Receive -= _length;
-    //                        Buffer.BlockCopy(Instance.BufferSize, _length, Instance.BufferSize, 0, Instance.Receive);
-    //                    }
-    //                    else
-    //                    {
-    //                        break;
-    //                    }
-    //                }
-    //            }
-    //        }
-    //        yield return null;
-    //    }
-    //}
+    IEnumerator PacketProc()
+    {
+        while (true)
+        {
+            if (InstanceValue.TCP.Available > 0)
+            {
+                byte[] _buffer = new byte[4096];
+                int _read = InstanceValue.TCP.Receive(_buffer, InstanceValue.TCP.Available, 0);
+                if (_read > 0)
+                {
+                    Buffer.BlockCopy(_buffer, 0, InstanceValue.BufferSize, InstanceValue.Receive, _read);
+                    InstanceValue.Receive += _read;
+                    while (true)
+                    {
+                        int _length = BitConverter.ToInt16(InstanceValue.BufferSize, 0);
+                        if (_length > 0 && InstanceValue.Receive >= _length)
+                        {
+                            ServerManager.ParsePacket(_length);
+                            InstanceValue.Receive -= _length;
+                            Buffer.BlockCopy(InstanceValue.BufferSize, _length, InstanceValue.BufferSize, 0, InstanceValue.Receive);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            yield return null;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (InstanceValue.TCP !=  null && InstanceValue.TCP.Connected)
+        {
+            ServerManager.Send("DISCONNECT");
+            Thread.Sleep(500);
+            InstanceValue.TCP.Close();
+        }
+        StopCoroutine(PacketProc());
+    }
 } 
