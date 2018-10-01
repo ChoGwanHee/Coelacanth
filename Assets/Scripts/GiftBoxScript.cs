d055ab0c152aa2166b1fbb0077d047e454a927ef
차진민 <wlsals3293@naver.com>
2018-10-01 21:48:43 +0900

    Stage2 fix

---
+++ Assets/Scripts/GiftBoxScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiftBoxScript : MonoBehaviour {
+
+    public Transform cover;
+
+    public float boomPower = 13.0f;
+
+    public ParticleSystem boomEfx;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        boomEfx.Stop(true);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            anim.SetBool("IsFire", true);
+        }
+        else if (Input.GetKeyDown(KeyCode.Y))
+        {
+            anim.SetBool("IsFire", false);
+            Rigidbody coverRb = cover.GetComponent<Rigidbody>();
+            coverRb.useGravity = false;
+        }
+    }
+    
+
+    public void Boom()
+    {
+        Rigidbody coverRb = cover.GetComponent<Rigidbody>();
+        coverRb.useGravity = true;
+        coverRb.velocity = new Vector3(0.0f, boomPower, 0.0f);
+        coverRb.angularVelocity = new Vector3(Random.Range(0.0f, 0.6f), Random.Range(0.0f, 0.6f), Random.Range(0.0f, 0.6f));
+
+        boomEfx.Play(true);
+    }
+
+}
